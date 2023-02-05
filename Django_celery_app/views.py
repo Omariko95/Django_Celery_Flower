c7eab7d8458c4c00bdcaea8fdea859324bc5d95b
Omar Moawad <114612179+Omariko95@users.noreply.github.com>
2023-02-05 16:23:38 -0500

    initial commit

--- Django_celery_app/views.py
+++ Django_celery_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .tasks import test
 
+# Create your views here.
+def home(request):
+  test.delay(15)
+  return HttpResponse("Hey there!")
 # Create your views here.
